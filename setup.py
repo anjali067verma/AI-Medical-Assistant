7c821e9954ae79f7a911b131ba1869434536f452
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2025-12-11 20:53:01 +0530

    Installed required packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="AI-Medical-Assistant",
+    version="0.1.0",
+    author="Anjali Verma",
+    author_email="av577340@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
